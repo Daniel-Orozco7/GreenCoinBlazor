9f0c5574a2ed7bedd6b4bf16e37dc0544c2750cb
smoscoso <smoscoso@ucundinamarca.edu.co>
2024-05-02 17:29:55 -0500

    Actualizacion vistas, favicon y base de datos

---
+++ Client/wwwroot/js/passwordToggle.js
@@ -0,0 +1,12 @@
+﻿// scripts.js
+function togglePassword(inputId) {
+    const inputElement = document.getElementById(inputId);
+    const currentType = inputElement.type;
+
+    // Cambiar el tipo de input basado en su estado actual
+    if (currentType === 'password') {
+        inputElement.type = 'text';
+    } else {
+        inputElement.type = 'password';
+    }
+}
